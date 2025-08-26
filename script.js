51617e22c1a439e5a63b30d4aecf9208f012889f
Debopri-yo <ghoshlipika53@gmail.com>
2025-08-26 13:08:25 +0530

    add bg color to the grids when mouse hovers

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function makeboxes(n){
     for(let i=0;i<n*n;i++){
         const box = document.createElement('div');
         box.className='cell';
+      const cellsize=(960/n);
+      box.style.width=cellsize+"px";
+      box.style.height=cellsize+"px";
+box.addEventListener("mouseover", function () {
+  box.style.backgroundColor = "black";
+});
         container.appendChild(box);
     }
 }
